5ed357e0c009f984edf9c07d9dc8ef960a0eb858
fatih yılmaz <fatihksks_@hotmail.com>
2020-07-31 23:44:51 +0300

    Admin yazar listeleme eklendi.

---
+++ BookEcommerce/Areas/Admin/Controllers/AuthorController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookEcommerce.Areas.Admin.Models;
+using BookEcommerce.Services.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookEcommerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AuthorController : Controller
+    {
+        private readonly AuthorService _authorService;
+        public AuthorController(AuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+        /// <summary>
+        /// Author listeleme Action
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            var entities = _authorService.GetAuthors();
+            var model = new List<AdminAuthorVM>();
+
+            foreach (var item in entities)
+            {
+                model.Add(new AdminAuthorVM()
+                {
+                    Id = item.Id,
+                    FullName = $"{item.FirstName} {item.LastName}"
+                });
+            }
+            return View(model);
+        }
+    }
+}
